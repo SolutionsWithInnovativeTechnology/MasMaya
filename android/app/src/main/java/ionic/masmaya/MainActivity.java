308f658251f0b1be29f7dcefe9b109439b536468
Saúl <sauln559@gmail.com>
2025-01-06 02:51:40 -0500

    inicio maya

---
+++ android/app/src/main/java/ionic/masmaya/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.masmaya;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.masmaya',
+  appName: 'masmaya',
+  webDir: 'www'
+};
+
+export default config;
